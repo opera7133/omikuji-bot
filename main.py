499552c9b1107fc6f1f2aec4de0e939f4a25f01d
wamo <ainznino@pm.me>
2025-05-05 13:41:02 +0900

    gemini update

--- main.py
+++ main.py
@@ -80,8 +80,8 @@ async def omikuji(interaction: discord.Interaction):
     # おみくじを引く
     fortune = secrets.choice(list(fortunes.keys()))
     flavor_res = gemini.models.generate_content(
-        model='gemini-2.0-flash-001',
-        contents=f"「{fortune}」という運勢のフレーバーテキストを日本語で一行だけ生成してください。出力は「{fortunes[fortune]}」のようにしてください。")
+        model='gemini-2.5-flash-preview-04-17',
+        contents=f"「{fortune}」という運勢のフレーバーテキストを日本語で一行だけ生成してください。出力は「{fortunes[fortune]}」のように、文章だけにしてください。出力がこれである必要はありません。")
     flavor = flavor_res.text
 
     # データベースに記録
